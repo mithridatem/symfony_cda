010fa7bcd3f9c147b72fc7da7fe107e9094d91a2
mithridatem <mathieumithridate@adrar-formation.com>
2023-04-12 12:21:03 +0200

    base projet_cda

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    #[Route('/article', name: 'app_article')]
+    public function index(): Response
+    {
+        return $this->render('article/index.html.twig', [
+        ]);
+    }
+}
