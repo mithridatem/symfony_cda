d142eecb48761fbd7f876a60ef8cf8c3879783da
mithridatem <mathieumithridate@adrar-formation.com>
2023-04-12 14:15:41 +0200

    exercice 1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+     #[Route('/{nbr1}/{nbr2}', name: 'app_home_param')]
+    public function index2($nbr1, $nbr2): Response
+    {
+        return $this->render('home/index2.html.twig', [
+            'nbr1'=> $nbr1,
+            'nbr2'=> $nbr2,
+        ]);
+    }
+ 
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
