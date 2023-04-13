739413c66b80fb428c4df45b9da0ef4ef9eae77c
mithridatem <mathieumithridate@adrar-formation.com>
2023-04-13 15:17:35 +0200

    update fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,11 +7,13 @@ use Doctrine\Persistence\ObjectManager;
 use Faker;
 use App\Entity\Categorie;
 use App\Entity\Article;
+use App\Entity\User;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {   
         $tabCats=[];
+        $userTab=[];
         $faker = Faker\Factory::create('fr_FR');
         for($i=0; $i<10; $i++){
             $cat = new Categorie();
@@ -19,11 +21,22 @@ class AppFixtures extends Fixture
             $manager->persist($cat);
             $tabCats[]= $cat;
         }
+        for($i=0; $i<5; $i++){
+            $user = new User();
+            $user->setEmail($faker->email());
+            $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+            $user->setPassword(password_hash('1234', PASSWORD_DEFAULT));
+            $user->setNom($faker->lastName());
+            $user->setPrenom($faker->firstName());
+            $manager->persist($user);
+            $userTab[]= $user;
+        }
         for($i=0; $i<10; $i++){
             $article = new Article();
             $article->setTitre($faker->words(3, true));
             $article->setContenu($faker->sentence(5));
             $article->setDate(new \DateTimeImmutable($faker->date('Y-m-d')));
+            $article->setUser($userTab[$faker->numberBetween(0, 4)]);
             $manager->persist($article);
         }
         $manager->flush();
