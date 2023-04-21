178a9fb5ee7b47122f8db5e8472d0fccc66ceb38
mithridatem <mathieumithridate@adrar-formation.com>
2023-04-21 15:34:59 +0200

    suppression article

--- public/asset/script/script.js
+++ public/asset/script/script.js
@@ -1,6 +1,8 @@
 //récupérer la zone ou on va afficher les articles
 let container = document.querySelector('#container');
 let url = 'https://localhost:8000/api/article/all';
+let url2 = 'https://localhost:8000/api/article/delete/';
+let charge = false;
 //écrire le script fetch
 const getArticle = fetch(url)
     .then(async response => {
@@ -18,8 +20,8 @@ const getArticle = fetch(url)
                 //parcour du json
                 data.forEach(obj => {
                     console.log(obj);
-                    let article = document.createElement('div');
-                    article.setAttribute('id', obj.id);
+                    const article = document.createElement('div');
+                    article.setAttribute('class', 'article');
                     container.appendChild(article);
                     const titre = document.createElement('h1');
                     titre.textContent =obj.titre;
@@ -27,9 +29,38 @@ const getArticle = fetch(url)
                     contenu.textContent =obj.contenu ;
                     const date = document.createElement('p');
                     date.textContent = obj.date.substring(0,10);
+                    const icone = document.createElement('i');
+                    icone.setAttribute('class', 'fa-solid fa-trash-can delete');
+                    icone.setAttribute('id', obj.id);
                     article.appendChild(titre);
                     article.appendChild(contenu);
                     article.appendChild(date);
+                    article.appendChild(icone);
+                    charge = true;
+                    icone.addEventListener('click', ()=>{
+                        console.log(icone.id);
+                        //fetch suppression 
+                        fetch(url2+icone.id,
+                            {method :'DELETE'})
+                        .then(async response1 => {
+                            //vérification du code erreur du serveur (BDD hs)
+                            if(response1.status == 500){
+                                //affichage de l'erreur
+                                alert('le serveur est en maintenance');
+                            }
+                            else{
+                                //récupérer le json de la suppression
+                                const dataSup = await response1.json();
+                                if(response1.status == 200){
+                                    article.remove();
+                                    alert(dataSup.erreur);
+                                }
+                                if(response1.status == 400){
+                                    alert(dataSup.erreur);
+                                }
+                            }
+                        })
+                    })
                 });
             }
             //cas ou il n'y a pas d'article
