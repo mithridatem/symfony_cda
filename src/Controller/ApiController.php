0e5910f0e62dfc3c0ea714dd0815045a77fbebac
mithridatem <mathieumithridate@adrar-formation.com>
2023-05-30 17:01:48 +0200

    JWT Token

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+use App\Service\ApiRegister;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+class ApiController extends AbstractController
+{
+    #[Route('/api', name: 'app_api')]
+    public function index(): Response
+    {
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+        ]);
+    }
+    #[Route('/api/register', name:'app_api_register')]
+    public function getToken(Request $request, UserRepository $repo,
+        UserPasswordHasherInterface $hash, ApiRegister $apiRegister){
+        //récupération du paramètre email
+        $mail = $request->query->get('email');
+        //récupération du paramètre password
+        $password = $request->query->get('password');
+        //test si le paramétre mail n'est pas saisi
+        if(!$mail OR !$password){
+            return $this->json(['Error'=>'informations absentes'], 400,['Content-Type'=>'application/json',
+            'Access-Control-Allow-Origin'=> '*'] );
+        }
+        //test si le compte est authentifié
+        if($apiRegister->authentification($hash,$repo,$mail,$password)){
+            //récupération de la clé de chiffrement
+            $secretKey = $this->getParameter('token');
+            //génération du token
+            $token = $apiRegister->genToken($mail, $secretKey, $repo);
+            //Retourne le JWT
+            return $this->json(['Token_JWT'=>$token], 200, ['Content-Type'=>'application/json',
+            'Access-Control-Allow-Origin'=> '*']);
+        }
+        //test si le compte n'est pas authentifié (erreur mail ou password)
+        else{
+            return $this->json(['Error'=>'Informations de connexion incorrectes'], 400, ['Content-Type'=>'application/json',
+            'Access-Control-Allow-Origin'=> '*']);
+        }
+    }
+    #[Route('api/testToken', name:'app_api_testToken')]
+    public function testToken(ApiRegister $apiRegister, Request $request){
+        //récupération du token
+        $jwt = substr($request->server->get('HTTP_AUTHORIZATION'),7);
+        //récupération de la clé de chiffrement
+        $secretKey = $this->getParameter('token');
+        //récupération de la vérification du token
+        $verif = $apiRegister->verifyToken($jwt, $secretKey);
+        //dd($verif);
+        //test sinon retourne l'erreur du token
+        if($verif===true){
+            return $this->json(['Accés authorisé'], 200, ['Content-Type'=>'application/json',
+            'Access-Control-Allow-Origin'=> '*']);
+        }
+        //test si le token est valide
+        else{
+            return $this->json(['Error'=>$verif], 400, ['Content-Type'=>'application/json',
+            'Access-Control-Allow-Origin'=> '*']);
+        }
+    }
+
+}
---
+++ src/Service/ApiRegister.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Service;
+use App\Repository\UserRepository;
+use App\Service\Utils;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+    class ApiRegister{
+        //fonction pour tester l'authentification
+        public function authentification(UserPasswordHasherInterface $hash,
+        UserRepository $repo, $email, $password){
+            //nettoyage du password
+            $password = Utils::cleanInputStatic($password);
+            //Récupération du compte
+            $recup = $repo->findOneBy(['email'=>Utils::cleanInputStatic($email)]);
+            //test si le compte existe
+            if($recup){
+                //test si le password est valide
+                if(!$hash->isPasswordValid($recup, $password)){
+                    return false;
+                }
+                //test sinon si le password est incorrect
+                else{
+                    return true;
+                }
+            }
+            //test sinon le compte n'existe pas
+            else{
+                return false;
+            }
+            
+        }
+        //fonction pour générer le token JWT
+        public function genToken($mail,$secretKey,$repo){
+            //construction du JWT
+            require_once('../vendor/autoload.php');
+            //Variables pour le token
+            $issuedAt   = new \DateTimeImmutable();
+            $expire     = $issuedAt->modify('+2 minutes')->getTimestamp();
+            $serverName = "your.domain.name";
+            $username   = $repo->findOneBy(['email'=>$mail])->getNom();
+            //Contenu du token
+            $data = [
+                'iat'  => $issuedAt->getTimestamp(),         // Timestamp génération du token
+                'iss'  => $serverName,                       // Serveur
+                'nbf'  => $issuedAt->getTimestamp(),         // Timestamp empécher date antérieure
+                'exp'  => $expire,                           // Timestamp expiration du token
+                'userName' => $username,                     // Nom utilisateur
+            ];
+            //retourne le JWT token encode
+            $token = JWT::encode(
+                $data,
+                $secretKey,
+                'HS512'
+            );
+            return $token;
+        }
+        //fonction pour véfifier si le token JWT est valide
+        public function verifyToken($jwt, $secretKey){
+            require_once('../vendor/autoload.php');
+            try {
+                //Décodage du token
+                $token = JWT::decode($jwt, new Key($secretKey, 'HS512'));
+                return true;
+            } catch (\Throwable $th) {
+                return $th->getMessage();
+            }
+        }
+    }
